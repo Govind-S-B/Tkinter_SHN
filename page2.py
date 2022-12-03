be807bff3722d35d55d46bdead5f26479e0cab90
Aquila <reenacottage@gmail.com>
2022-12-03 22:35:15 +0530

    minor text alignments

--- page2.py
+++ page2.py
@@ -1,5 +1,4 @@
 from tkinter import *
-# from tkinter.ttk import *
 import tkinter as tk
 import sqlite3 as sq
 from PIL import Image, ImageFont, ImageDraw, ImageTk
@@ -11,7 +10,6 @@ root.minsize(600, 400)
 
 canvas = Canvas(root, width=600, height=400, bg='white')
 canvas.pack(anchor=tk.CENTER, expand=True)
-
 
 conn = sq.connect('report.db')
 
@@ -41,19 +39,17 @@ for i in result:
     draw = ImageDraw.Draw(bg)
     date_ = i[0].split("-")
     date = date_[2]+"-"+date_[1]+"-"+date_[0]
-    draw.text((70, 30), date, fill="black", anchor="ms", font=font1)
+    draw.text((75, 30), date, fill="black", anchor="ms", font=font1)
     draw.text((50, 60), i[1], fill="white", anchor="ms", font=font2)
     bg.save("./cards/" + i[0]+".png")
 
     photo = PhotoImage(file="./cards/" + i[0]+".png")
     label = Label(canvas, image=photo)
     label.image = photo
-    label.grid(column=c, row=1, sticky=S)
+    label.grid(column=c, row=r, sticky=S)
 
-    # Label(canvas, text=i[1], bd=5,
-    #       relief='groove').grid(column=c, row=1, sticky=S)
     c += 1
-    if c >= 4:
+    if c > 4:
         r += 1
         c = 1
 
