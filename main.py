08d982ac946c0fe469078c41f29138ad81565e76
Govind S B <b.s.dnivog@gmail.com>
2022-12-03 18:47:33 +0530

    rotation

--- main.py
+++ main.py
@@ -4,34 +4,27 @@ from PIL import Image,ImageTk
 root=Tk()
 root.geometry("1000x600")
 
-img1=ImageTk.PhotoImage(Image.open("./src/color1.jpg"))
-img2=ImageTk.PhotoImage(Image.open("./src/color2.jpg"))
-img3=ImageTk.PhotoImage(Image.open("./src/color3.jpg"))
-img4=ImageTk.PhotoImage(Image.open("./src/color4.jpg"))
-img5=ImageTk.PhotoImage(Image.open("./src/color5.jpg"))
-
+seed_img = Image.open("./src/test.jpg")
 l=Label(root,font="bold")
 l.pack()
 
 x=1
 
+show_img = seed_img.rotate(0.1*x)
+img1=ImageTk.PhotoImage(show_img)
+
 def move():
     global x
-    if x==6:
-        x=1
-    if x==1:
-        l.config(image=img1)
-    elif x==2:
-        l.config(image=img2)
-    elif x==3:
-        l.config(image=img3)
-    elif x==4:
-        l.config(image=img4)
-    elif x==5:
-        l.config(image=img5)
+    global seed_img
+    global img1
+
+    show_img = seed_img.rotate(1*x)
+    img1=ImageTk.PhotoImage(show_img)
+    
+    l.config(image=img1)
 
     x+=1
-    root.after(33, move)
+    root.after(1, move)
 
 move()
 mainloop()
